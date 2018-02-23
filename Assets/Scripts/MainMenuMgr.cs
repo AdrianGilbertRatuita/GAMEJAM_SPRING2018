e417f6d6a66fd32442c14c92ae2e0135856ccbeb
apod1010 <32077837+apod1010@users.noreply.github.com>
2018-02-23 12:18:33 -0600

    Health Rotation
    orb rotation and menu flow

---
+++ Assets/Scripts/MainMenuMgr.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuMgr : MonoBehaviour {
+
+    public void StartButtonClick()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+}
