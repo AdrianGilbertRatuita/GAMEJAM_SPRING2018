6fc6787603a69d41bb52ca1dfd73a887201f7c4f
Adrian Gilbert Ratuita <adrianratuita@gmail.com>
2018-02-23 13:25:20 -0600

    tOTEM

--- DragonBloodTree/Assets/Scripts/Interactables/GameTotem.cs
+++ DragonBloodTree/Assets/Scripts/Interactables/GameTotem.cs
@@ -6,14 +6,25 @@ using UnityEngine.SceneManagement;
 public class GameTotem : MonoBehaviour
 {
 
+    // Next Level (Scene Number) to load when Orbs is collected
     public int NextLevel;
-    public int OrbRequirement;
+    //
+    private int OrbRequirement;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+
+        Initialize();
+
 	}
+
+    void Initialize()
+    {
+
+        GetOrbCount();
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -21,6 +32,27 @@ public class GameTotem : MonoBehaviour
 		
 
 	}
+
+    private void GetOrbCount()
+    {
+
+        // Gets Total Number of Orbs in Level
+        GameObject[] Check;
+        Check = GameObject.FindGameObjectsWithTag("Collectibles");
+
+        for (int i = 0; i < Check.Length; i++)
+        {
+
+            if (Check[i].gameObject.name == "Orb")
+            {
+
+                OrbRequirement++;
+
+            }
+
+        }
+
+    }
 
     private void OnTriggerStay(Collider other)
     {
