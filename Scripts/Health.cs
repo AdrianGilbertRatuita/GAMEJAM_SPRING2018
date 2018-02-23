1326775589a2666afebbf61af0269b930d6571f4
apod1010 <32077837+apod1010@users.noreply.github.com>
2018-02-23 13:15:27 -0600

    Add files via upload

---
+++ Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    [SerializeField]
+    GameObject Orb;
+    [SerializeField]
+    GameObject Player;
+    [SerializeField]
+    float rotateSpeed = 20f;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Orb.transform.RotateAround(Player.transform.position, Vector3.back, rotateSpeed * Time.deltaTime);
+    }
+}
---
+++ Scripts/Interactables/Totem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Totem : IInteractable
+{
+    public bool Conditions(Protagonist P)
+    {
+
+        return false;
+
+    }
+
+    public bool Conditions()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Execute()
+    {
+
+    }
+
+}
